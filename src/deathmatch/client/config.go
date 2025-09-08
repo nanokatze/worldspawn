1ef504d18649922293c22960a9fb3a2dedf71569
Caterina Shablia <nanokatze@gmail.com>
2025-09-08 21:53:19 +0200

    deathmatch: stick to strongly typed Config

--- src/deathmatch/client/config.go
+++ src/deathmatch/client/config.go
@@ -5,28 +5,10 @@ import (
 	"sync/atomic"
 )
 
-// TODO: replace with map[string]any + bunch of helper methods for conversions?
-// TODO: our input needs to be action sets like in steam input, how do we store
-// that?
-
 type Config struct {
 	DisableCosmeticOffset bool
 	DontInterpolate       bool
 }
-
-/*
-type Config2 map[string]any
-
-func confGet[T any](conf Config2) (T, bool) {
-	k := reflect.TypeFor[T]().String()
-	if v, ok := conf[k]; ok {
-		if v, ok := v.(T); ok {
-			return v, ok
-		}
-	}
-	return *new(T), false
-}
-*/
 
 var (
 	configMu sync.Mutex // only locked when modifying
