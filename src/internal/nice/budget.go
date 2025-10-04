5026a4f024c668a41553230f7bdb2265f05b43d6
Caterina Shablia <nanokatze@gmail.com>
2025-10-04 22:25:35 +0200

    nice: introduce outOfMemoryError

--- src/internal/nice/budget.go
+++ src/internal/nice/budget.go
@@ -5,22 +5,31 @@ import (
 	"reflect"
 )
 
+type outOfMemoryError struct {
+	n      int
+	budget int
+}
+
+func (e *outOfMemoryError) Error() string {
+	return fmt.Sprintf("out of memory: %d (remaining budget %d)", e.n, e.budget)
+}
+
 type Budget struct {
-	remaining int
+	n int
 }
 
 func (budget *Budget) reset(n int) {
-	budget.remaining = n
+	budget.n = n
 }
 
 func (budget *Budget) Account(n int) error {
 	if n < 0 {
 		panic("uhh?")
 	}
-	if budget.remaining < n {
-		return fmt.Errorf("out of memory: %d (remaining budget %d)", n, budget.remaining)
+	if budget.n < n {
+		return &outOfMemoryError{n, budget.n}
 	}
-	budget.remaining -= n
+	budget.n -= n
 	return nil
 }
 
