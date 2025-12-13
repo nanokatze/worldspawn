21e14840ec4839dcc61108c364ef421a08311fe7
Caterina Shablia <nanokatze@gmail.com>
2025-12-13 04:31:10 +0100

    pathtracer: move EnqueueUpdate to scene.go
    scene_update.go is on its way out

--- src/internal/pathtracer/scene_update.go
+++ src/internal/pathtracer/scene_update.go
@@ -3,7 +3,6 @@ package pathtracer
 import (
 	"worldspawn/geometry-go"
 	"worldspawn/gpu"
-	"worldspawn/gpu/vk"
 )
 
 // TODO: merge this file into scene
@@ -59,111 +58,4 @@ func (s *SceneUpdate) Transform(i int, t float32) geometry.Mat4x4 {
 		B = A.Mul4x4(B)
 	}
 	return B
-}
-
-// Not sure whether this should take t. We might wanna make it a separate
-// method.
-// TODO: do not do any host state management here but delegate things to the
-// user where applicable. E.g. for materials we'll want the user to be p
-// explicit about them (assign materials indices or w/e)
-func (scene *Scene) EnqueueUpdate(jq *gpu.JobQueue, dirty *SceneUpdate, t float32) {
-	// TODO: update sky device-side too? That would make things extra neat and
-	// fun.
-	// TODO: hard-require sky?
-	if dirty.Sky != nil {
-		scene.lightAccel.env = dirty.Sky.SamplingDescriptor().WithSampler(scene.sampler)
-	} else {
-		scene.lightAccel.env = gpu.SamplingViewWithSampler{}
-	}
-
-	// BUG: writes should happen after all commands in jq complete. We should
-	// just put things into a staging buffer and perform a copy on the device.
-
-	var instanceCount int
-	var emissiveInstanceCount int
-
-	materialParamsHost := scene.materialParams.Value()
-	accelInstancesHost := scene.accelInstances.Value()
-	emissiveInstancesHost := scene.lightAccel.emissiveInstances.Value()
-	for instanceIdx, instance := range dirty.Instance {
-		// TODO: is this necessary?
-		// TODO: actually we might have instances at index 0: this would happen
-		// if the entity at index 0 is in the next generation. Maybe we should
-		// just forbid index 0 in our entities.
-		if instanceIdx == 0 || instance.Transform == 0 {
-			accelInstancesHost[instanceIdx] = gpu.AccelInstance{}
-			continue
-		}
-
-		// Should be done on the device
-		instanceTransform := dirty.Transform(instanceIdx, t)
-
-		// TODO: outline into a func
-		var A [3][4]float32
-		for i := range A {
-			for j := range A[i] {
-				A[i][j] = instanceTransform[i][j]
-			}
-		}
-
-		mesh := dirty.Mesh[instanceIdx]
-
-		for partIdx, part := range mesh.Parts {
-			materialInstance := dirty.Materials[instanceIdx][partIdx]
-
-			materialParamsHost[instanceIdx*scene.maxPartsPerMesh+partIdx] = materialParams{
-				Program: materialInstance.Material.program,
-
-				// TODO: make Mesh device-accessible so we don't have to do these redundant copies every time
-				MeshPart: meshPart2{
-					Triangles:    gpu.SliceData(part.IndexBuffer),
-					NumTriangles: uint32(gpu.SliceLen(part.IndexBuffer)),
-					PosBuffer:    gpu.SliceData(part.PosBuffer),
-					Normals:      gpu.SliceData(part.NormalBuffer),
-				},
-
-				Args: materialInstance.Args,
-			}
-
-			// TODO: we should build an emissive blas and when instancing it
-			// we'll enable/disable geometries (by specifying emission power for
-			// those geometries)
-			if materialInstance.Material.emissive {
-				emissiveInstancesHost[emissiveInstanceCount] = emissiveInstance{
-					transform:             A,
-					originalInstanceIndex: uint32(instanceIdx),
-					originalGeometryIndex: uint32(partIdx),
-				}
-				emissiveInstanceCount++
-			}
-		}
-
-		// TODO: is this necessary?
-		var accel gpu.UnsafePointer
-		if mesh != nil {
-			accel = mesh.accel.Data
-		}
-
-		accelInstancesHost[instanceIdx] = gpu.AccelInstance{
-			Transform:         A,
-			InstanceIDAndMask: pack24_8(0, uint32(instance.Mask)),
-			SBTOffsetAndFlags: pack24_8(uint32(instanceIdx*scene.maxPartsPerMesh), 0),
-			Accel:             accel,
-		}
-
-		instanceCount = max(instanceCount, instanceIdx+1)
-	}
-
-	scene.lightAccel.emissiveInstanceCount = emissiveInstanceCount
-
-	scene.accel.EnqueueBuild(jq,
-		&gpu.AccelBuildConfig{
-			Type: vk.ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR,
-			Inputs: []gpu.AccelBuildInput{
-				&gpu.AccelBuildInputInstances{
-					Instances:     gpu.SliceData(scene.accelInstances),
-					InstanceCount: uint32(instanceCount),
-				},
-			},
-		})
 }
