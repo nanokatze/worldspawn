c4fd77f93bf1fffe2646b5d9d087d081d8535eb7
Caterina Shablia <nanokatze@gmail.com>
2026-01-25 13:43:13 +0100

    forgor to add bloom shader

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -7,4 +7,5 @@ OUTDIR=shaders
 SLANG=../slang/build/Release/bin/slangc
 SLANG_FLAGS="-target spirv -profile spirv_1_6 -fvk-use-entrypoint-name -fvk-use-c-layout -matrix-layout-row-major -capability vk_mem_model"
 
+$SLANG $SLANG_FLAGS -o "$OUTDIR/postprocess_bloom.spv" internal/postprocess/bloom.slang
 $SLANG $SLANG_FLAGS -o "$OUTDIR/pathtracer_scene_render.spv" internal/pathtracer/scene_render.slang
