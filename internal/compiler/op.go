f283fb794ae4a686b7f95e20c7616e6484206bb2
Caterina Shablia <nanokatze@gmail.com>
2026-02-19 21:37:45 +0100

    compiler: simplify Op

--- internal/compiler/op.go
+++ internal/compiler/op.go
@@ -1,30 +1,21 @@
 package compiler
 
-type Op struct{ id int32 }
+import "unique"
 
-// TODO: introduce OpMap[T] for efficiently mapping Op->T
-// TODO: we might want an inverse as well, map[string]Op
-var opNames = make(map[Op]string)
+type Op struct{ name unique.Handle[string] }
 
 type Validator func(typ Type, imm any, args ...*Class)
 
 var validators = make(map[Op]Validator)
 
-var nextOpID int32 = 1
-
-// TODO: also provide immediate type? This would let us feed into the parser
-// immediately and nicely.
+// TODO: also provide immediate type
+// TODO: guard against redefining an op
 func DefOp(name string, validator Validator) Op {
-	id := nextOpID
-	nextOpID++
-	if id == 0 {
-		panic("overflow")
-	}
-
-	op := Op{id}
-	opNames[op] = name
+	op := Op{unique.Make(name)}
 	validators[op] = validator
 	return op
 }
 
-func (op Op) String() string { return opNames[op] }
+func (op Op) String() string {
+	return op.name.Value()
+}
