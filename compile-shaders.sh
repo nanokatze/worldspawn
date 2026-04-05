2b52082652f9abcb01b3ad093bef468e1a293cb5
Caterina Shablia <nanokatze@gmail.com>
2026-04-05 22:57:32 +0200

    fixup sfx3d compile-shaders.sh

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -7,6 +7,7 @@ OUTDIR=shaders
 SLANG=../slang/build/Release/bin/slangc
 SLANG_FLAGS="-target spirv -profile spirv_1_6 -fvk-use-entrypoint-name -fvk-use-c-layout -matrix-layout-row-major -capability vk_mem_model"
 
+$SLANG $SLANG_FLAGS -o "$OUTDIR/experiments_sfx3d_main.spv" experiments/sfx3d/main.slang
 $SLANG $SLANG_FLAGS -o "$OUTDIR/geometry_skinning.spv" internal/geometry/skinning.slang
 $SLANG $SLANG_FLAGS -o "$OUTDIR/pathtracer_scene_render.spv" internal/pathtracer/scene_render.slang
 $SLANG $SLANG_FLAGS -o "$OUTDIR/postprocess_bloom.spv" internal/postprocess/bloom.slang
