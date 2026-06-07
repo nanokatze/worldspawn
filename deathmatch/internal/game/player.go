9f61b38d79894fa0362af6f2b59d439748e15b4d
Caterina Shablia <nanokatze@gmail.com>
2026-06-07 20:05:37 +0200

    deathmatch: implement death and respawn
    Very basic for now.
    
    Closes #153

--- deathmatch/internal/game/player.go
+++ deathmatch/internal/game/player.go
@@ -5,38 +5,44 @@ import (
 )
 
 type Player struct {
+	ControlledCharacter ecs.ID
+
 	Score struct {
 		Kills  int32
 		Deaths int32
 	}
-
-	ControlledCharacter ecs.ID
 }
 
 func (Player) entity() {}
 
-// TODO: delete, client should figure this out by itself
+// TODO: returning ecs.ID is kinda meh, ideally we'd return a pile of data that
+// can be fed straight into pathtracer
+// TODO: make this a method on the World?
 func (player Player) Camera(world *World) ecs.ID {
-	if player.ControlledCharacter == 0 {
-		return 0 // TODO: get rid of this and make the callers care
+	char, ok := SceneGetEntity[Gladiator](world, player.ControlledCharacter)
+	if !ok {
+		return 0
 	}
-	char := mustOk(SceneGetEntity[Gladiator](world, player.ControlledCharacter))
 	return char.FirstPersonCamera
 }
 
 func (world *World) HandleInput(playerID ecs.ID, cmd TimestampedInputCmd, info *UpdateParams) {
 	player := mustOk(SceneGetEntity[Player](world, playerID))
-	char := mustOk(SceneGetEntity[PlayableCharacter](world, player.ControlledCharacter))
-	char.HandleInput(world, player.ControlledCharacter, cmd, info)
+	if char, ok := SceneGetEntity[PlayableCharacter](world, player.ControlledCharacter); ok {
+		char.HandleInput(world, player.ControlledCharacter, cmd, info)
+	} else {
+		if !info.Speculating {
+			player.ControlledCharacter = world.spawnGladiator(world.findSpawnPoint(), info)
+			world.Entity.Set(playerID, player)
+		}
+	}
 }
 
+// TODO: should be called something like CreatePlayer or something. We only
+// really need to poke this when new client joins. We could also rename Player
+// to Client or Connection or User or idk.
 func (world *World) SpawnPlayer(info *UpdateParams) ecs.ID {
 	player := world.CreateEntity(info)
-
-	char := world.spawnGladiator(world.findSpawnPoint(), info)
-	world.Entity.Set(player, Player{
-		ControlledCharacter: char,
-	})
-
+	world.Entity.Set(player, Player{})
 	return player
 }
