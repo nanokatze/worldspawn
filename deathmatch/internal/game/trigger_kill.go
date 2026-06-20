166a924a8e17b11f7eeae7a9b3d87e917e2dcd8e
Caterina Shablia <nanokatze@gmail.com>
2026-06-20 02:37:43 +0200

    deathmatch: add trigger_kill
    This is a simple deletion trigger. It probably should not be
    called trigger_kill but something else tbh.

---
+++ deathmatch/internal/game/trigger_kill.go
@@ -0,0 +1,17 @@
+package game
+
+import (
+	"worldspawn/internal/ecs"
+)
+
+func init() {
+	scripts["trigger_kill"] = scriptFuncs{
+		ContactAdded: func(world *World, entity1, entity2 ecs.ID, info *UpdateParams) {
+			world.Delete.Set(entity2, struct{}{})
+			info.Logger.Info("entity entered the trigger", "us", entity1, "them", entity2)
+		},
+		ContactRemoved: func(world *World, entity1, entity2 ecs.ID, info *UpdateParams) {
+			info.Logger.Info("entity left the trigger", "us", entity1, "them", entity2)
+		},
+	}
+}
