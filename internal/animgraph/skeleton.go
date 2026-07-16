9d418117b297c3fec349da0aef4fa18ccc948687
Caterina Shablia <nanokatze@gmail.com>
2026-07-16 11:46:49 +0200

    fixup f21afb83683f177793c3102c2fc44e3fd282be90

--- internal/animgraph/skeleton.go
+++ internal/animgraph/skeleton.go
@@ -1,11 +1,20 @@
 package animgraph
 
-import "worldspawn/internal/gmath"
+import (
+	"unique"
+
+	"worldspawn/internal/gmath"
+)
+
+// TODO: introduce skeleton builder to simplify loader code
+
+type SkeletonBuilder struct {
+}
 
 // TODO: make the internals private probably?
 type Skeleton struct {
-	JointNames   []string
-	JointByName_ map[string]int
+	JointNames   []unique.Handle[string]
+	JointByName_ map[unique.Handle[string]]int
 
 	Parent          []int
 	Children        [][]int
@@ -14,7 +23,7 @@ type Skeleton struct {
 	ParentRelative  []gmath.Affine3f32
 }
 
-func (s *Skeleton) JointByName(name string) int {
+func (s *Skeleton) JointByName(name unique.Handle[string]) int {
 	if i, ok := s.JointByName_[name]; ok {
 		return i
 	}
