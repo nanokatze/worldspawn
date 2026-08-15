39ebcf6cf657de0b4739d102f6ea896676f6796f
Caterina Shablia <nanokatze@gmail.com>
2026-08-15 02:08:57 +0200

    deathmatch: name the remaining basis elements

--- deathmatch/internal/game/defs.go
+++ deathmatch/internal/game/defs.go
@@ -2,10 +2,14 @@ package game
 
 import "worldspawn/internal/gmath"
 
-var forward = gmath.Vec3f32{0, 1, 0}
+var (
+	forward = gmath.Vec3f32{0, 1, 0}
+	right   = gmath.Vec3f32{1, 0, 0}
+	up      = gmath.Vec3f32{0, 0, 1}
+)
 
-// TODO: name these after planes, i.e. e01 should probably be named horizon and
-// e12 meridian or idk.
+// TODO: name these after planes, i.e. e01 should probably be named
+// horzion, or horizonCCW.
 
 var e01 = gmath.Rot3AToB(gmath.Vec3f32{1, 0, 0}, gmath.Vec3f32{0, 1, 0})
 var e12 = gmath.Rot3AToB(gmath.Vec3f32{0, 1, 0}, gmath.Vec3f32{0, 0, 1})
